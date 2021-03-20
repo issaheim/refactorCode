9cad130610609d7838aa8ad1706f27ab43a387b6
issaheim <75672568+issaheim@users.noreply.github.com>
2021-03-20 10:17:17 +0100

    solved second task

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 // Kod 1:
+console.log("Kod 1:");
 let cars = ['volvo', 'saab', 'bmw', 'tesla'];  
 for (let car in cars){
   console.log(cars[car]);
 }
+
+// Kod 2:
+console.log("Kod 2:");
+
+let fruitCars = {
+  "cars": ['volvo', 'saab', 'bmw', 'tesla'],
+  "fruits": ['apple', 'banana', 'orange']
+};
+
+for (let fruitCar in fruitCars){
+ let arrays = fruitCars[fruitCar];
+ for (let array in arrays){
+   console.log(arrays[array]);
+ };
+};
